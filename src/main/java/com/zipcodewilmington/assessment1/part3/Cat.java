a98d195cb75954e17ba063bfccd6a983cff5d447
David Trombello <david@MacBook-Pro-8.localdomain>
2019-10-28 16:01:30 -0400

    dog and cat tests pass

--- src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Cat.java
@@ -8,6 +8,9 @@ public class Cat extends Pet {
      * @param name name of this Cat
      * @param age age of this Cat
      */
+    //String name = "CatName";
+    //Integer age = 0;
+
 
     public Cat(String name, Integer age) {
         this.name = name;
@@ -18,7 +21,8 @@ public class Cat extends Pet {
      * @param age age of this Cat
      */
     public Cat(Integer age) {
-        this.age = age;
+        this.name = "Cat name";
+        this.age = Integer.MAX_VALUE;;
     }
 
     /**
@@ -36,8 +40,11 @@ public class Cat extends Pet {
      * age is 0
      */
     public Cat() {
+        this.name = "Cat name";
+        this.age=0;
     }
-//All the above are constructors
+
+    //All the above are constructors
 
     /**
      * @return meow as a string
--- src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
+++ src/main/java/com/zipcodewilmington/assessment1/part3/Dog.java
@@ -9,6 +9,8 @@ public class Dog extends Pet {
      * @param age age of this dog
      */
     public Dog(String name, Integer age) {
+        this.name = name;
+        this.age = age;
 
     }
 
@@ -16,12 +18,15 @@ public class Dog extends Pet {
      * @param age age of this dog
      */
     public Dog(Integer age) {
+        this.age=age;
+        this.name="Dog name";
     }
 
     /**
      * @param name name of this dog
      */
     public Dog(String name) {
+        this.name = name;
 
     }
 
@@ -32,12 +37,14 @@ public class Dog extends Pet {
      * age is 0
      */
     public Dog() {
+        this.name = "Dog name";
+        this.age=0;
     }
 
     /**
      * @return bark as a string
      */
     public String speak() {
-        return null;
+        return "Bark";
     }
 }
