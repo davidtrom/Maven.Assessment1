3e48df18e9225b01d17ae1d80f2d0db68fba7216
David Trombello <david@MacBook-Pro-8.localdomain>
2019-10-30 12:16:05 -0400

    jump test pass

--- src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
+++ src/main/java/com/zipcodewilmington/assessment1/part4/Jumper.java
@@ -6,6 +6,22 @@ public class Jumper {
      * Complete the function below.
      */
     public int jumps(int k, int j) {
-        return -1;
+
+        //k is the flag height
+        //j is the jump height
+        return k / j + k % j;
+
+
+//        if(k%j==0)
+//        { return (k/j);}
+//        else {
+//            return (k/j) +1;
+//        }
+//        if (j>k) {
+//            return 1;
+//        }
+//        }
     }
 }
+
+
